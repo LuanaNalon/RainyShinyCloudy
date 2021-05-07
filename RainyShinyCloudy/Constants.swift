dfa793aa3042da70e01fd195755ba4b471e1bf8b
Luana Nalon <luananalon@hotmail.com>
2021-05-07 16:44:21 +0100

    API Weather integrate, create a CurrentWeather class and constants

---
+++ RainyShinyCloudy/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RainyShinyCloudy
+//
+//  Created by Luana Nalon on 07/05/2021.
+//
+
+import Foundation
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat=39.7436"
+let LONGITUDE = "&lon=-8.8071"
+let APP_ID = "&appid="
+let API_KEY = "657061082f9e08c718e57b593923a4a0"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)\(LONGITUDE)\(APP_ID)\(API_KEY)"
