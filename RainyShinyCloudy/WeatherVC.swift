a7510114c0531af0675f8dad18bffc618e19853c
Luana Nalon <luananalon@hotmail.com>
2021-05-07 13:11:12 +0100

    Cocoapods and Alamofire instaled, inicial page design added

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Luana Nalon on 06/05/2021.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatheImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
